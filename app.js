5c1e39e27abdefc369718f1ed1ecf60d3ef2e01a
Wassim Sellami <wassim.sellami@supcom.tn>
2023-01-23 11:12:57 +0100

    adding simple token based auth

--- app.js
+++ app.js
@@ -9,31 +9,43 @@ dotenv.config();
 let PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () =>{
-    console.log(`Server is up and running on ${Port}`)
+    console.log(`Server is up and running on ${PORT}`)
 });  
   
 
 // Post request to generate JWT and sends it in response
-    app.post("user/generateToken", async(req, res) =>  {
+    app.post("/user/generateToken", async(req, res) =>  {
         try{
-            
+            let jwtSecretKey = process.env.JWT_SECRET_KEY;
+            let data = {
+                "user_id": 12,
+                time: Date()
+            };
+            const token = jwt.sign(data, jwtSecretKey);
+            res.send(token);
         }
         catch(e){
+
             res.end(e.message ||e.toString());
         }
     });
 
-app.post('/test', async function (req, res) {
-    try {  
-      const user = 'User';
-      const query = 'SELECT [Password] as password FROM [Table] where [User] = ' + SqlString.escape(user);
-      const pool = await sql.connect(dbConfig);
-      const result = await pool.request()
-        .query(querys);
-      const password = result.recordset[0].password;
-      console.log(password);
-      res.end(password);
-    } catch (e) {
-      res.end(e.message || e.toString());
+
+// Get request containing the JWT token in the header and sends back validation resposne
+
+app.get('/user/validateToken', (req, res) =>{
+    let jwtSecretKey = process.env.JWT_SECRET_KEY;
+    try{
+        const token = req.headers.authorization.split(' ')[1]; 
+        // Bearer token
+        const verified = jwt.verify(token, jwtSecretKey);
+        if(verified){
+            res.send("You are authenticated !");
+        }else{
+            return res.status(401).send(error);
+        }
     }
-  });
+    catch(err){
+        return res.status(401).send(err);
+    }
+});
